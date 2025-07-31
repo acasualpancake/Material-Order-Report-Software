f18ea134df3c5b99a5912a8e5191017bff7ec8d1
LostBoyHubble <malte-grossmann@gmx.de>
2025-07-31 13:03:39 +0200

    [MORS-4] Added app component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// Global style imports
+import './App.css';
+
+// View imports
+import MainView from "./views/MainView";
+
+// Funcational imports
+
+// Component export
+export default () => {
+  return  (<>
+    <MainView />
+  </>);
+}
